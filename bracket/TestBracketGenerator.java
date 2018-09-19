98365a56f431189c5be4ae9136c497e53b76510f
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-09-19 11:28:15 -0400

    Update TestBracketGenerator.java

--- bracket/TestBracketGenerator.java
+++ bracket/TestBracketGenerator.java
@@ -10,6 +10,34 @@ public class TestBracketGenerator {
         this.teams = teams;
     }
 
-    public TestBracket getBracket() { ;
+    public TestBracket getBracket() { 
+    }
+    
+     /**
+     * generates the winning bracket
+     * @return the winning bracket
+     */
+    private ArrayList<Match>[] generateWinningBracket() {
+
+        // there will always be ceil(lg(N)) rounds
+        int numberOfRounds = (int)Math.ceil(Math.log(teams.size())/Math.log(2));
+
+        // populate the winning bracket;
+        ArrayList<Match>[] winningBracket = new ArrayList[numberOfRounds];
+        for (int i = 0; i < numberOfRounds; i++) {
+            for (int j = 0; j < Math.pow(2, i); j++) {
+                winningBracket[0].add(new Match());
+            }
+        }
+
+        // populate the matches
+        Iterator<Team> teamIterator = teams.iterator();
+        int index = 0;
+        while (teamIterator.hasNext()) {
+            winningBracket[numberOfRounds - 1].get(index).addTeam(teamIterator.next());
+            index = (index + 1) % winningBracket[numberOfRounds - 1].size();
+        }
+
+        return winningBracket;
     }
 }
